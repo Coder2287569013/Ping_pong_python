471dc9fc18c480eb5fabe804a3f2ce42de8049d3
Coder2287569013 <101576295+Coder2287569013@users.noreply.github.com>
2023-03-25 16:57:39 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
+#importing library
 import pygame
-
 pygame.init()
+#window and game clock
 w, h = 640, 480
 sc = pygame.display.set_mode((w,h))
 clock = pygame.time.Clock()
 fps = 60
 score1 = 0
 score2 = 0
+#sounds
+sound = pygame.mixer.Sound("Beep.ogg")
+pygame.mixer.music.load("background_music.mp3")
+pygame.mixer.music.set_volume(0.4)
+pygame.mixer.music.play(-1)
+#fonts
 font = pygame.font.SysFont("Arial", 40)
 font2 = pygame.font.Font("font/DTM-Sans.otf", 54)
-
-sound = pygame.mixer.Sound("Hit.ogg")
-
+#images
 back_img = pygame.image.load("background.png")
 pong_img = pygame.image.load("pong2.png")
 ball_img = pygame.image.load("ball.png")
-
+#texts
 win1_text = font2.render("Player1 win", True, (10,255,0))
 win2_text = font2.render("Player2 win", True, (10,255,0))
-
+#General class, class Ball, class Player
 class GameObject:
     def __init__(self,x,y,width,height,img):
         self.rect = pygame.Rect(x,y,width,height)
@@ -62,13 +67,15 @@ class Player(GameObject):
             self.rect.y -= self.speed
         if keys[pygame.K_DOWN] and self.rect.bottom <= h:
             self.rect.y += self.speed
-
+#Creation of prototypes of classes
 player = Player(0,h/2-50,50,70, pong_img)
 player2 = Player(w-50,h/2-50,50,70, pygame.transform.flip(pong_img, True, False))
 ball = Ball(w/2, h/2, 16,16,ball_img)
+#condition of game, pause and win
 game = True
-pause = False
+pause = True
 win = False
+#main loop
 while game:
     sc.fill((0,0,0))
     sc.blit(back_img, (0,0))
@@ -82,7 +89,12 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            game = False
+            game = False,
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE and not pause:
+                pause = True
+            else:
+                pause = False
     if not pause and not win:
         ball.movement()
         player.movement()
@@ -104,10 +116,10 @@ while game:
         ball.rect.x, ball.rect.y = player.rect.right,h/2-16
         ball.dx, ball.dy = -1,-1
         pause = True
-    if score1 >= 15:
+    if score1 >= 10:
         sc.blit(win1_text, (w/2-150, h/2-40))
         win = True
-    if score2 >= 15:
+    if score2 >= 10:
         sc.blit(win2_text, (w/2-150, h/2-40))
         win = True
     key = pygame.key.get_pressed()
