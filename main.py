cb6f41b78a953a2605430a6c375036cdc132a45e
Coder2287569013 <101576295+Coder2287569013@users.noreply.github.com>
2023-03-30 21:46:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ ball_img = pygame.image.load("ball.png")
 #texts
 win1_text = font2.render("Player1 win", True, (10,255,0))
 win2_text = font2.render("Player2 win", True, (10,255,0))
+
 #General class, class Ball, class Player
 class GameObject:
     def __init__(self,x,y,width,height,img):
@@ -36,12 +37,13 @@ class Ball(GameObject):
         self.dx, self.dy = 1, -1
         self.speed = 4
     def movement(self):
-        self.rect.x += self.speed*self.dx
-        self.rect.y += self.speed*self.dy
-        if self.rect.y <= 0:
-            self.dy = 1
-        if self.rect.y >= h-self.rect.height:
-            self.dy = -1
+        if pause != True:
+            self.rect.x += self.speed*self.dx
+            self.rect.y += self.speed*self.dy
+            if self.rect.y <= 0:
+                self.dy = 1
+            if self.rect.y >= h-self.rect.height:
+                self.dy = -1
     def collision(self, player, player2):
         if self.rect.colliderect(player.rect) and self.dx == -1:
             self.dx = 1
@@ -72,8 +74,9 @@ player = Player(0,h/2-50,50,70, pong_img)
 player2 = Player(w-50,h/2-50,50,70, pygame.transform.flip(pong_img, True, False))
 ball = Ball(w/2, h/2, 16,16,ball_img)
 #condition of game, pause and win
+pause = False
+game_start = False
 game = True
-pause = True
 win = False
 #main loop
 while game:
@@ -89,17 +92,18 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            game = False,
+            game = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE and not pause:
                 pause = True
-            else:
+            elif event.key == pygame.K_ESCAPE and pause:
                 pause = False
-    if not pause and not win:
-        ball.movement()
-        player.movement()
-        player2.movement2()
-        ball.collision(player, player2)
+            if event.key == pygame.K_RETURN and not game_start: game_start = True
+            if event.key == pygame.K_RETURN and win:
+                score1 = 0
+                score2 = 0
+                ball.speed = 4
+                win = False
     if ball.rect.x >= w+ball.rect.width:
         ball.speed = 4
         player.rect.x, player.rect.y = 0,h/2-50
@@ -107,7 +111,7 @@ while game:
         score1 += 1
         ball.rect.x, ball.rect.y = player2.rect.left-ball.rect.width,h/2-16
         ball.dx, ball.dy = 1,1
-        pause = True
+        game_start = False
     if ball.rect.x <= -ball.rect.width:
         ball.speed = 4
         player.rect.x, player.rect.y = 0,h/2-50
@@ -115,23 +119,23 @@ while game:
         score2 += 1
         ball.rect.x, ball.rect.y = player.rect.right,h/2-16
         ball.dx, ball.dy = -1,-1
-        pause = True
+        game_start = False
+    if game_start and not win and not pause:
+        ball.movement()
+        player.movement()
+        player2.movement2()
+        ball.collision(player, player2)
     if score1 >= 10:
         sc.blit(win1_text, (w/2-150, h/2-40))
         win = True
+        ball.dx, ball.dy = 1,1
+        ball.rect.x, ball.rect.y = player2.rect.left-ball.rect.width,h/2-16
+        player2.rect.x, player2.rect.y = w-50,h/2-50
     if score2 >= 10:
         sc.blit(win2_text, (w/2-150, h/2-40))
         win = True
-    key = pygame.key.get_pressed()
-    if key[pygame.K_RETURN] and pause: pause = False
-    if key[pygame.K_RETURN] and win:
-        score1 = 0
-        score2 = 0
-        ball.speed = 4
-        player.rect.x, player.rect.y = 0,h/2-50
+        ball.dx, ball.dy = -1,-1
+        ball.rect.x, ball.rect.y = player.rect.right,h/2-16
         player2.rect.x, player2.rect.y = w-50,h/2-50
-        ball.rect.x, ball.rect.y = w/2, h/2
-        ball.dx, ball.dy = 1,-1
-        win = False
     pygame.display.update()
     clock.tick(fps)
